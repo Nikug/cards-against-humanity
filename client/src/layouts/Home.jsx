e8b834afb6291cd7739d7600bbd964d7fb04c596
Maija <maijaaaw@gmail.com>
2020-10-21 15:03:40 +0300

    Move buttons in correct places

--- client/src/layouts/Home.jsx
+++ client/src/layouts/Home.jsx
@@ -15,14 +15,16 @@ export function Home(props) {
                 </h1>
                 <div className="create-or-join-game-buttons">
                     <div className="container">
-                        <div className="text">Luo uusi peli</div>
-                        <button className="button" onClick={startNewGame}>Luo peli</button>
+                        <div className="text">Luo uusi peli, johon voit kutsua kaverisi mukaan</div>
+                        <div className="input-and-button-container">
+                        <button className="button primary" onClick={startNewGame}>Luo peli</button>
+                        </div>
                     </div>
                     <div className="container border">
                         <div className="text">Liity olemassa olevaan peliin syöttämällä linkki</div>
                         <div className="input-and-button-container">
-                            <input type="text" className="input"/> 
-                            <input type="button" value="Liity" className="button primary" onClick={() => joinExistingGame(url)} />
+                            <input type="text" className="input" placeholder="existing-game-name-44"/> 
+                            <input type="button" value="Liity peliin" className="button primary" onClick={() => joinExistingGame(url)} />
                         </div>
                     </div>
                 </div>
