f126eb0c297bc4775c7cd912591363c071b69bba
Maija <maijaaaw@gmail.com>
2020-10-21 14:42:12 +0300

    Add background image

--- client/src/App.js
+++ client/src/App.js
@@ -70,6 +70,7 @@ class App extends Component {
                         />
                     </Switch>
                 </Router>
+                <div className="copyrights">&copy; 2020</div>
             </div>
         );
     }
--- client/src/layouts/Home.jsx
+++ client/src/layouts/Home.jsx
@@ -10,16 +10,19 @@ export function Home(props) {
     if (urlIsEmpty) { 
         return (
             <div className="home-wrapper">
+                <h1 className="welcome-text">
+                    Tervetuloa pelaamaan kortteja ihmiskuntaa vastaan!
+                </h1>
                 <div className="create-or-join-game-buttons">
                     <div className="container">
                         <div className="text">Luo uusi peli</div>
-                        <input type="button" value="Luo Peli" className="button" onClick={startNewGame} />
+                        <button className="button" onClick={startNewGame}>Luo peli</button>
                     </div>
                     <div className="container border">
                         <div className="text">Liity olemassa olevaan peliin syöttämällä linkki</div>
                         <div className="input-and-button-container">
                             <input type="text" className="input"/> 
-                            <input type="button" value="Liity" className="button" onClick={() => joinExistingGame(url)} />
+                            <input type="button" value="Liity" className="button primary" onClick={() => joinExistingGame(url)} />
                         </div>
                     </div>
                 </div>
