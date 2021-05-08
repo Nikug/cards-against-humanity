be59ad701b28d600f3b0b723fa070e58fe34d1bd
Maija <maijaaaw@gmail.com>
2021-05-08 18:17:47 +0300

    refactor button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { classNames } from '../helpers/classnames';
 
 import Icon from './icon';
@@ -8,42 +8,50 @@ export const BUTTON_TYPES = {
     GREEN: 2,
 };
 
-export const Button = React.forwardRef((props, ref) => {
-    const { additionalClassname, callback, callbackParams, disabled, fill, icon, iconPosition, text, type } = props;
-    const noText = text === undefined || text === null || text.length === 0;
+const getClassNameString = (additionalClassname, disabled, fill, type) => {
+    return classNames('button', additionalClassname, {
+        fill: fill === 'fill',
+        'fill-vertical': fill === 'fill-vertical',
+        'fill-horizontal': fill === 'fill-horizontal',
+        disabled,
+        primary: type === BUTTON_TYPES.PRIMARY,
+        green: type === BUTTON_TYPES.GREEN,
+    });
+};
+
+const getOnclickFunction = (callback, callbackParams, disabled) => {
+    return (e) => {
+        if (disabled || !callback) {
+            return;
+        }
+
+        callback(callbackParams);
+
+        if (e.target.nodeName !== 'BUTTON') {
+            e.target.parentElement.blur();
+        }
+
+        e.target.blur();
+    };
+};
+
+const getButtonIcon = (icon, iconAfterText, noText) => {
+    return <Icon name={icon} className={classNames('button-icon', { 'no-margin-right': noText || iconAfterText, 'after-text': iconAfterText })} />;
+};
+
+export const Button = React.forwardRef(({ additionalClassname, callback, callbackParams, disabled, fill, icon, iconPosition, text, type }, ref) => {
+    const noText = text == null || text.length === 0;
+    const iconAfterText = iconPosition === 'after';
+
+    const classNameString = useMemo(() => getClassNameString(additionalClassname, disabled, fill, type), [additionalClassname, disabled, fill, type]);
+    const onClickFunction = useMemo(() => getOnclickFunction(callback, callbackParams, disabled), [callback, callbackParams, disabled]);
+    const buttonicon = useMemo(() => getButtonIcon(icon, iconPosition), [icon, iconAfterText, noText]);
 
     return (
-        <button
-            ref={ref}
-            className={classNames('button', additionalClassname, {
-                fill: fill === 'fill',
-                'fill-vertical': fill === 'fill-vertical',
-                'fill-horizontal': fill === 'fill-horizontal',
-                disabled,
-                primary: type === BUTTON_TYPES.PRIMARY,
-                green: type === BUTTON_TYPES.GREEN,
-            })}
-            onClick={(e) => {
-                if (disabled || !callback) {
-                    return;
-                }
-                callback(callbackParams);
-                if (e.target.nodeName !== 'BUTTON') {
-                    e.target.parentElement.blur();
-                }
-                e.target.blur();
-            }}
-        >
-            {icon && iconPosition !== 'after' && (
-                <Icon
-                    name={icon}
-                    className={classNames('button-icon', {
-                        'no-margin-right': noText,
-                    })}
-                />
-            )}
+        <button ref={ref} className={classNameString} onClick={onClickFunction}>
+            {!iconAfterText && buttonicon}
             {text}
-            {icon && iconPosition === 'after' && <Icon name={icon} className={classNames('button-icon', 'after-text')} />}
+            {iconAfterText && buttonicon}
         </button>
     );
 });
