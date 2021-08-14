b9ed9b0feb9fe412fd08647d263b2c31159550e4
nikugronberg <niku.gronberg@student.lut.fi>
2021-08-14 11:42:27 +0300

    add changet text-to-speech test

--- server/tests/changeTextToSpeech.spec.ts
+++ server/tests/changeTextToSpeech.spec.ts
@@ -59,4 +59,39 @@ describe("Change text-to-speech", () => {
         const game: Game = await mockSet.mock.results[0].value;
         expect(game.players[0].useTextToSpeech).toBe(false);
     });
+
+    it("Handles values that are not boolean by casting to boolean", async () => {
+        const mockSet = mockSetGame();
+        const newGame = newGameTemplate(mockGameId);
+        newGame.stateMachine.jumpTo("readingCards");
+        newGame.client.state = newGame.stateMachine.state;
+        const player = createPlayer("host", true);
+        player.useTextToSpeech = true;
+        newGame.players = [player];
+        mockGetGame(newGame);
+
+        await changePlayerTextToSpeech(
+            ioMock,
+            mockGameId,
+            "host",
+            "maybe" as unknown as boolean,
+            pgClientMock
+        );
+
+        expect(mockSet).toHaveBeenCalledTimes(1);
+        const game: Game = await mockSet.mock.results[0].value;
+        expect(game.players[0].useTextToSpeech).toBe(true);
+
+        await changePlayerTextToSpeech(
+            ioMock,
+            mockGameId,
+            "host",
+            undefined as unknown as boolean,
+            pgClientMock
+        );
+
+        expect(mockSet).toHaveBeenCalledTimes(2);
+        const game2: Game = await mockSet.mock.results[1].value;
+        expect(game2.players[0].useTextToSpeech).toBe(false);
+    });
 });
