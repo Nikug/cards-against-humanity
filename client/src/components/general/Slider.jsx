6ee75f4e64433614a465fe6f053fb74018d336af
apezkin <ape.zkin@gmail.com>
2021-09-06 20:56:30 +0300

    Slider component is ready to use

---
+++ client/src/components/general/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Slider = ({ title, value, changeCallback, min = 0, max = 100 }) => {
+    return (
+        <div className="slider">
+            <div className="title">{title}</div>
+            <div className="slider-container">
+                <div className="slider-value">{value}</div>
+                <input type="range" min={min} max={max} value={value} onChange={changeCallback}></input>
+            </div>
+        </div>
+    );
+};
--- client/src/components/usersettings.js
+++ client/src/components/usersettings.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tippy from '@tippyjs/react';
 
 import { useTranslation } from 'react-i18next';
 import { translateCommon } from '../helpers/translation-helpers';
 import Icon from './general/Icon';
+import { Slider } from './general/Slider';
+
+const DEFAULT_VOLUME = 50;
 
 export const UserSettings = () => {
     const { t } = useTranslation();
+
+    const [radioVolValue, setRadioVolValue] = useState(DEFAULT_VOLUME);
+    const [text2SpeechVolValue, setText2SpeechVolValue] = useState(DEFAULT_VOLUME);
+    const [soundEffectVolValue, setSoundEffectVolValue] = useState(DEFAULT_VOLUME);
 
     return (
         <Tippy
@@ -18,8 +25,19 @@ export const UserSettings = () => {
             interactive={true}
             arrow={true}
             content={
-                <div className="language-selector-wrapper">
+                <div className="user-settings-wrapper">
                     <div className="title">{translateCommon('settings', t)}</div>
+                    <Slider title={translateCommon('radioVolume', t)} value={radioVolValue} changeCallback={(event) => setRadioVolValue(event.target.value)} />
+                    <Slider
+                        title={translateCommon('textToSpeechVolume', t)}
+                        value={text2SpeechVolValue}
+                        changeCallback={(event) => setText2SpeechVolValue(event.target.value)}
+                    />
+                    <Slider
+                        title={translateCommon('soundEffectVolume', t)}
+                        value={soundEffectVolValue}
+                        changeCallback={(event) => setSoundEffectVolValue(event.target.value)}
+                    />
                 </div>
             }
         >
