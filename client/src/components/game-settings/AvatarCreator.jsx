1ff0c42f532b539d7ccec2e05c4e29ee5d24cf5d
petterigit <33552590+petterigit@users.noreply.github.com>
2021-09-17 14:04:21 +0300

    Avatar menu v0.2 Make grid look better Refactor component structure TODO: SetCurrentAvatar() rendering

--- client/src/components/game-settings/AvatarCreator.jsx
+++ client/src/components/game-settings/AvatarCreator.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { translateUnderWork } from '../../helpers/translation-helpers';
 import Icon from '../general/Icon';
-import { AvatarPicker } from './AvatarPicker';
+
+const defaultAvatar = {
+    hatType: 0,
+    eyeType: 0,
+    mouthType: 0
+};
+
+const maxAvatarTypes = {
+    hats: 5,
+    eyes: 5,
+    mouths: 5, 
+}
+
+const avatarImg = "https://cdn.shopify.com/s/files/1/0220/9497/0980/products/image_e0191ed4-1fcc-4e99-a811-0624189386bc.png?v=1558095871";
 
 export const AvatarCreator = () => {
     const { t } = useTranslation();
-    const currentAvatar = 0;
-    const isDisabled = false;
-    const iconClassnames = 'icon-margin-right';
+    const [currentAvatar, setCurrentAvatar] = useState(defaultAvatar);
 
-
-    const avatarChoices = {
-        heads : ["https://m.media-amazon.com/images/I/71Jy2kGzpxS._AC_UL1500_.jpg",
-                    "https://previews.123rf.com/images/polkan/polkan1111/polkan111100017/11246127-illustration-of-a-santa-head.jpg"],
-        torsos : ["https://previews.123rf.com/images/wernerimages/wernerimages1504/wernerimages150400098/39044962-smooth-muscular-male-torso.jpg",
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTgGv2vNTDmTipiGqNn03CoiG2_u1Ua32OJw&usqp=CAU"],
-        legs : ["https://static.independent.co.uk/s3fs-public/thumbnails/image/2012/11/03/21/10-shapelylegs.jpg?width=1200", 
-                    "https://npr.brightspotcdn.com/dims4/default/c86f8e4/2147483647/strip/true/crop/5520x3684+0+0/resize/880x587!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fwuwm%2Ffiles%2F201911%2FAdobeStock_245471467.jpeg"],
-    }
+    const changeAvatar = (type, dir) => {
+        let newAvatar = currentAvatar
+        if (dir === "next") {
+            newAvatar[type] = Math.max(newAvatar[type] + 1, maxAvatarTypes[type]);
+            setCurrentAvatar(newAvatar);
+            console.log("Set avatar");
+        } else if ( dir === "prev") {
+            newAvatar[type] = Math.min(newAvatar[type] - 1, 0);
+            setCurrentAvatar(newAvatar);
+            console.log("Set avatar");
+        } else {
+            return;
+        }
+    };
 
     return( 
         <div className="avatar-creator-container">
@@ -26,14 +43,29 @@ export const AvatarCreator = () => {
                 {translateUnderWork('avatarCreator', t)}
             </div>
             
-            <div className="avatar-picker-1"> 
-                <AvatarPicker choices={avatarChoices.heads}/>
+            <span className="hat-text">Hat: {currentAvatar.hatType} </span>
+            <div className="arrow-left-1">
+                <Icon name="arrow_back_ios" className="" onClick={() => changeAvatar("hatType", "prev")} />   
             </div>
-            <div className="avatar-picker-2"> 
-                <AvatarPicker choices={avatarChoices.torsos}/>
+            <span className="eyes-text">Eyes: {currentAvatar.eyeType}</span>
+            <div className="arrow-left-2">
+                <Icon name="arrow_back_ios" className="" onClick={() => changeAvatar("eyeType", "prev")} />   
             </div>
-            <div className="avatar-picker-3"> 
-                <AvatarPicker choices={avatarChoices.legs}/>
+            <span className="mouth-text">Mouth: {currentAvatar.mouthType}</span>
+            <div className="arrow-left-3">
+                <Icon name="arrow_back_ios" className="" onClick={() => changeAvatar("mouthType", "prev")} />   
             </div>
+            <div className="avatar">
+                <img src={avatarImg}/>
+            </div>
+            <div className="arrow-right-1">
+                <Icon name="arrow_forward_ios" className="" onClick={() => changeAvatar("hatType", "next")} />   
+            </div>
+            <div className="arrow-right-2">
+                <Icon name="arrow_forward_ios" className="" onClick={() => changeAvatar("eyeType", "next")} />   
+            </div>
+            <div className="arrow-right-3">
+                <Icon name="arrow_forward_ios" className="" onClick={() => changeAvatar("mouthType", "next")} />   
+            </div>   
         </div>);
 };
