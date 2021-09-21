f54e9c00bc4fcdc16a154ae7bed72055ddcb2998
Maija <maijaaaw@gmail.com>
2021-09-21 22:34:13 +0300

    remove console log

--- client/src/components/general/Icon.jsx
+++ client/src/components/general/Icon.jsx
@@ -7,9 +7,6 @@ const getIconClassNames = (className, color, onClick, disabled) => {
 };
 
 export default function Icon({ name, className, color, disabled, onClick }) {
-    if (name === 'skip_previous') {
-        console.log({ disabled });
-    }
     const classNameString = useMemo(() => getIconClassNames(className, color, onClick, disabled), [className, color, onClick, disabled]);
 
     return (
