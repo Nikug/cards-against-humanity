28961ddbd009d993298dc09d06f7d5af8aa62409
Maija <maijaaaw@gmail.com>
2021-09-22 21:23:33 +0300

    add button to copy joining link

---
+++ client/src/components/game-settings/CopyGameLinkButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { copyTextToClipboard } from "../../helpers/clipboardhelpers";
+import { translateCommon } from "../../helpers/translation-helpers";
+import { Button, BUTTON_TYPES } from "../general/Button";
+
+export const CopyGameLinkButton = () => {
+    const {t} = useTranslation();
+
+    const copyGameLink = () => {
+        copyTextToClipboard(window.location.href);
+    }
+
+    return (
+        <Button callback={copyGameLink} text={translateCommon('copyJoiningLink', t)} icon={'content_copy'} type={BUTTON_TYPES.PRIMARY}/>
+    )
+}
