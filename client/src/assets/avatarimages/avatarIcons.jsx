3db1a4e1f4cc7ed4ff2f8a6e3f4d1d112388b542
petterigit <33552590+petterigit@users.noreply.github.com>
2021-11-08 13:49:24 +0200

    Change SVG images to PNG

--- client/src/assets/avatarimages/avatarIcons.jsx
+++ client/src/assets/avatarimages/avatarIcons.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/*
 import { ReactComponent as KoalaHat } from './KoalaEars.svg';
 import { ReactComponent as KoalaEyes } from './KoalaEyes.svg';
 import { ReactComponent as KoalaMouth } from './KoalaMouth.svg';
@@ -7,7 +8,6 @@ import { ReactComponent as MehHat } from './MehHat.svg';
 import { ReactComponent as MehEyes } from './MehEyes.svg';
 import { ReactComponent as MehMouth } from './MehMouth.svg';
 
-import avatarIcon from './koala.png';
 
 const hats = [null, <KoalaHat />, <MehHat />];
 const eyes = [null, <KoalaEyes />, <MehEyes />];
@@ -40,6 +40,60 @@ export const Avatar = () => {
     return (
         <div className={'avatar-background-image'}>
             <img src={avatarIcon} />
+        </div>
+    );
+};
+
+export const Hat = ({ type }) => {
+    return <AvatarOverLayImageWrapper>{getHat(type)}</AvatarOverLayImageWrapper>;
+};
+
+export const Eyes = ({ type }) => {
+    return <AvatarOverLayImageWrapper>{getEyes(type)}</AvatarOverLayImageWrapper>;
+};
+
+export const Mouth = ({ type }) => {
+    return <AvatarOverLayImageWrapper>{getMouth(type)}</AvatarOverLayImageWrapper>;
+};
+
+*/
+
+import avatarBackground from './koala-512x512.png';
+import hat1 from './hat-512x512.png';
+import eyes1 from './eyes-512x512.png';
+import mouth1 from './mouth-512x512.png';
+
+const hats = [null, hat1];
+const eyes = [null, eyes1];
+const mouths = [null, mouth1];
+
+export const maxAvatarTypes = {
+    // -1 because of null option
+    hatType: hats.length - 1,
+    eyeType: eyes.length - 1,
+    mouthType: mouths.length - 1,
+};
+
+const getHat = (type) => {
+    return <img src={hats[type]} alt="avatar-hat" />;
+};
+
+const getEyes = (type) => {
+    return <img src={eyes[type]} alt="avatar-eyes" />;
+};
+
+const getMouth = (type) => {
+    return <img src={mouths[type]} alt="avatar-mouth" />;
+};
+
+const AvatarOverLayImageWrapper = ({ children }) => {
+    return <div className={'avatar-overlay-image'}>{children}</div>;
+};
+
+export const Avatar = () => {
+    return (
+        <div className={'avatar-background-image'}>
+            <img src={avatarBackground} alt="avatar" />
         </div>
     );
 };
